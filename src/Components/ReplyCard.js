2244c45b08b23d564f62d45f674fea125fb771ba
Filip Koniuszewski <filip7446@gmail.com>
2022-04-22 03:39:25 +0200

    implemented reply section

---
+++ src/Components/ReplyCard.js
@@ -0,0 +1,44 @@
+﻿import React from 'react';
+import plusIcon from "../Assets/Images/icon-plus.svg";
+import minusIcon from "../Assets/Images/icon-minus.svg";
+import replyIcon from "../Assets/Images/icon-reply.svg";
+import "../Style/CommentCard.css";
+
+function ReplyCard(props) {
+
+    let userImage = require(`../Assets/Images/avatars/${props.user.image.png}`)
+    
+    return (
+        <div className="comment-card reply-card">
+            <div className="stats-section">
+                <img src={plusIcon} alt="" />
+                <div className="stats-count">
+                    {props.score}
+                </div>
+                <img src={minusIcon} alt="" />
+            </div>
+            <div className="comment-section">
+                <div className="user">
+                    <div className="user-info">
+                        <img src={userImage} alt="" />
+                        <span className="user-name">
+                                {props.user.username}
+                            </span>
+                        <span className="comment-date">
+                                {props.createdAt}
+                            </span>
+                    </div>
+                    <div className="reply">
+                        <img src={replyIcon} alt="" />
+                        <span>Reply</span>
+                    </div>
+                </div>
+                <div className="comment-content reply-content">
+                    <span className="replying-to">@{props.replyTo}</span>{props.content}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ReplyCard;
---
+++ src/Components/ReplySection.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import "../Style/ReplySection.css";
+import ReplyCard from "./ReplyCard";
+
+export default function ReplySection(props) {
+    return (
+        <div className="reply-section">
+            {props.replies.map((reply) => 
+                <ReplyCard 
+                    key={reply.id}
+                    content={reply.content}
+                    createdAt={reply.createdAt}
+                    score={reply.score}
+                    user={reply.user}
+                    replyTo={reply.replyingTo}/>)}
+        </div>
+    );
+}
